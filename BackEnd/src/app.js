d9ebb3ec1079e5ffd7bee7a5518b22155519869b
vinay42 <vinaythakor47@gmail.com>
2024-12-16 12:12:45 +0530

    change

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -8,9 +8,7 @@ app.use(cors({
     origin: ["http://localhost:5173","https://post-hive-lu8b.vercel.app"],
     // origin: 'http://localhost:5173',
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
-    exposedHeaders: ['set-cookie']
+    
 }))
 
 app.use(express.json({limit: "16kb"}))
