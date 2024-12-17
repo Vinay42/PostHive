4d2d947e7567b99b69f847096147222f40ebc838
vinay42 <vinaythakor47@gmail.com>
2024-12-17 12:01:55 +0530

    change

--- BackEnd/src/middlewares/multer.middleware.js
+++ BackEnd/src/middlewares/multer.middleware.js
@@ -1,18 +1,8 @@
-import fs from "fs";
 import multer from "multer";
-import path from "path";
-
-const tempDir = "./public/temp";
-
-// Ensure the directory exists
-if (!fs.existsSync(tempDir)) {
-  fs.mkdirSync(tempDir, { recursive: true });
-}
-
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./public/temp")
+      cb(null, "../../public/temp")
     },
     filename: function (req, file, cb) {
       
